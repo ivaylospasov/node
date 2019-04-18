eace7e688fbc0218300a49f6df6924e28e4c0a3e
ivaylospasov <ivaylo.pif@gmail.com>
2019-04-18 11:05:15 +0300

    Preparation for the website

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+app.set('view engine', 'pug');
+app.get('/', (req, res) => {
+  res.render('index', {
+  name: 'Ayo'
+});;
+});
+const server = app.listen(7000, () => {
+  console.log(`Express running → PORT ${server.address().port}`);
+});
